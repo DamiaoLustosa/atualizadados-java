16e26a8db141c3b11abcfc20c16b5ebb7bdd5b43
DamiaoLustosa <50031115+DamiaoLustosa@users.noreply.github.com>
2019-07-04 14:54:02 -0300

    Atualizando dados com JDBC
    Atualizando dados com JDBC

---
+++ jdbc4/src/aplication/Programa.java
@@ -0,0 +1,37 @@
+package aplication;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		PreparedStatement st = null;
+		
+		try {
+			conn = DB.getConnection();
+			st = conn.prepareStatement("UPDATE seller SET BaseSalary = BaseSalary + ?"
+					+ " WHERE (DepartmentId = ?)");
+			st.setDouble(1, 200.00);
+			st.setInt(2, 4);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Feito (done)! Linhas afetadas: " + rowsAffected);	
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+
+	}
+
+}
